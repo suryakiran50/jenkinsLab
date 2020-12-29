ee7772bb2ca1d8da3dbc45a5b2ee9ba027581355
codyblackwell2018 <41333234+codyblackwell2018@users.noreply.github.com>
2020-12-29 19:40:30 +0000

    JavaPractice
    Added java practice file

---
+++ Practice1.java
@@ -0,0 +1,11 @@
+package com.practice.jenkins;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	}
+
+}
