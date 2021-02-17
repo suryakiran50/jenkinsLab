d732c67bfc11b03f5680492239763aa472cef215
suryakiran50 <59191454+suryakiran50@users.noreply.github.com>
2021-02-17 12:53:16 +0530

    Update Practice1.java

--- Practice1.java
+++ Practice1.java
@@ -5,7 +5,7 @@ public class Practice1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
+		System.out.println("Hello World! testing jenkins");
 	}
 
 }
